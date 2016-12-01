46a9d7be0ef18a112d84bbb7840d44bda76e8b0b
blureze <blureze@gmail.com>
2016-12-01 12:38:24 +0800

    update login.py

--- login.py
+++ login.py
@@ -2,18 +2,10 @@
 import json
 from bs4 import BeautifulSoup
 
+
 def login(login_url, session_requests):
-	initial(login_url, session_requests)
-	logindata = read_secret()
-
-	formdata = {
-		'user[email]': logindata['email'], 
-		'user[password]': logindata['password'],
-		'authenticity_token': authenticity_token,
-		'n': hidden_n
-	}
-
-	result = send_form(formdata, login_url)
+	formdata = initial(login_url, session_requests)
+	result = send_form(formdata, login_url, session_requests)
 
 	return result	
 
@@ -24,17 +16,27 @@ def initial(login_url, session_requests):
 	authenticity_token = soup.find('input', {'name': 'authenticity_token'}).get('value')
 	hidden_n = soup.find('input', {'name': 'n'}).get('value')
 
+	logindata = read_secret()
+
+	formdata = {
+		'user[email]': logindata['email'], 
+		'user[password]': logindata['password'],
+		'authenticity_token': authenticity_token,
+		'n': hidden_n
+	}
+
+	return formdata	
+
 def read_secret():
-	with open('config/secret.json', 'r', encoding='utf-8') as data_file:
+	with open('config/secret.json', 'r') as data_file:
 	    data = json.load(data_file)
 
-	print data
 	logindata = {'email': data['email'], 
 				'password': data['password']}
 
 	return logindata
 
-def send_form(formdata, login_url):
+def send_form(formdata, login_url, session_requests):
 	result = session_requests.post(
 		login_url,
 		data = formdata
